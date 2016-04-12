543b12abdc9eb3f6818a66d4a694ea56c04c53e7
Gert Kremer <gkremer@schubergphilis.com>
2016-04-12 12:04:17 +0200

    suppress 'error' output during build

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ CURRENT_PWD=`pwd`
 export COPYFILE_DISABLE=true
 
 # Cleanup
-rm $CURRENT_PWD/eventgen.spl
+rm $CURRENT_PWD/eventgen.spl >/dev/null 2>&1
 
 # Create a build directory that we can use
 BUILD_DIR=.build/SA-Eventgen
@@ -14,13 +14,13 @@ BUILD_DIR_PARENT=.build
 mkdir -p $BUILD_DIR
 
 cp -R * $BUILD_DIR
-rm $BUILD_DIR/eventgen.spl >/dev/null 2>/dev/null
-rm -rf $BUILD_DIR/.* >/dev/null 2>/dev/null
-rm -rf $BUILD_DIR/node_modules >/dev/null 2>/dev/null
-find $BUILD_DIR -name '._*' -print | xargs rm
+rm $BUILD_DIR/eventgen.spl >/dev/null 2>&1
+rm -rf $BUILD_DIR/.* >/dev/null 2>&1
+rm -rf $BUILD_DIR/node_modules >/dev/null 2>&1
+find $BUILD_DIR -name '._*' -print -exec rm {} \;
 cd $BUILD_DIR_PARENT
 tar -czf $CURRENT_PWD/eventgen.spl SA-Eventgen
 cd $CURRENT_PWD
-rm -rf $BUILD_DIR
+rm -rf $BUILD_DIR >/dev/null 2>&1
 
 echo "Build Complete"
